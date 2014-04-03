d61f8ed655dac75eb666df70606a604d6c55b9f1
Etresoft <info@etresoft.com>
2014-04-03 19:10:54 -0400

    Initial Commit

---
+++ EtreSpell/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  EtreSpell
+//
+//  Created by John Daniel on 2014-04-03.
+//  Copyright (c) 2014 Etresoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
